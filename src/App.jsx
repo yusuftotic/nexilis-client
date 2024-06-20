b93a2cfdb4dc3a8451d630e7be038e1b9589d6d3
yusuftotic <yusuftotic@outlook.com>
2024-06-21 01:29:11 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Blog from './pages/blog/Blog.jsx';
+
+import './App.css';
+
+export default function App () {
+
+  return (
+    <div className="app">
+      <Blog />
+    </div>
+  )
+}
