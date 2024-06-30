b99cf99ece6428d1d7e673174a5ffe0295683c75
yusuftotic <yusuftotic@outlook.com>
2024-06-30 11:43:18 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://nexilis-api.vercel.app/api",
+  // baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json"
   },
